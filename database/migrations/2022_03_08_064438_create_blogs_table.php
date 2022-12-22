190ebe5caa0cf430e71227f4f8d4805c7919af56
Navjot Shrivastava <107606405+navjotcognicsys@users.noreply.github.com>
2022-12-22 15:43:41 +0530

    Add files via upload

---
+++ database/migrations/2022_03_08_064438_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('url')->nullable();
+            $table->text('review')->nullable();
+            $table->integer('position')->default(0);
+            $table->boolean('activated')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
